b4ee038c5eba7a0f699694cab1c7e2152f96f16c
Nolan Caudill <nolan@nolancaudill.com>
2010-12-05 15:08:29 -0800

    Foodspotting parser works

--- parsers/foodspotting.php
+++ parsers/foodspotting.php
@@ -1,8 +1,19 @@
 <?php
 
     function get_geodata($value) {
+
+        $url = "http://www.foodspotting.com/places/" . $value;
+        $page = file_get_contents($url);
+        
+        // Extract latitude
+        preg_match('#id="place_latitude".*value="(.*)"#U', $page, $matches);
+        $latitude = $matches[1];
+
+        preg_match('#id="place_longitude".*value="(.*)"#U', $page, $matches);
+        $longitude = $matches[1];
+
         return array(
-            "latitude" => "35.7",
-            "longitude" => "-135.7",
+            "latitude" => $latitude,
+            "longitude" => $longitude,
         );
     }
