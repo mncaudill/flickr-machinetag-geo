580ffffc371e8d12ccaac26b0edc0ff7e047c9f5
Nolan Caudill <nolan@nolancaudill.com>
2012-01-14 17:07:58 -0800

    This seems bulletproof...

--- src/parsers/foursquare.php
+++ src/parsers/foursquare.php
@@ -6,10 +6,10 @@
         $page = file_get_contents($url);
 
         // Extract latitude
-        preg_match('#<meta content="(.*)" property="og:latitude"#U', $page, $matches);
+        preg_match('#"lat":(.*),#U', $page, $matches);
         $latitude = $matches[1];
 
-        preg_match('#<meta content="(.*)" property="og:longitude"#U', $page, $matches);
+        preg_match('#"lng":(.*),#U', $page, $matches);
         $longitude = $matches[1];
 
         preg_match('#<meta content="(.*)" property="og:title"#U', $page, $matches);
